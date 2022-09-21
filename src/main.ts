c0eb39b1046c69f1f01e05178417f51118cf9b75
danillyamkin <danilwebdev@gmail.com>
2022-09-21 22:29:27 +0300

    Added app config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Глобальная настройка приложения
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+
+  // OpenAPI
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Application')
+    .setDescription('description')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  // Доступ к .env переменным
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get<number>('PORT') || 5000;
+
+  await app.listen(PORT, () => {
+    console.log(`Server started on ${PORT} port.`);
+  });
 }
 bootstrap();
